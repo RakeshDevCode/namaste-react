38ec7f44889e517b1825704083b1105ff858f787
rakes singh <rakeshdevcode@gmail.com>
2025-07-02 17:08:03 +0530

    update

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import RestaurantMenu from "./components/RestaurantMenu";
 import {createBrowserRouter, RouterProvider,Outlet,} from "react-router";
 import UserContext from "./utils/UserContext";
 import { Provider } from "react-redux";
-import appStore from "./utils/AppStore";
+import appStore from "./utils/appStore";
 import Cart from "./components/Cart";
 //import Grocery from "./components/Grocery";
 
